fd686a8ee674ba1122e1bb8eaec762e45019795a
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-19 15:46:52 +0300

    Create VersionC.cpp

---
+++ Workbook/Глава 2 - Основни структури за управление на изчислителния процес/Решени задачи/Задача 11/VersionC.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+int main() {
+	double x, y;
+	std::cout << "Enter the coordinates of the point: ";
+	std::cin >> x >> y;
+	if (!std::cin) {
+		std::cout << "Error! You have entered wrong data!" << std::endl;
+		return 1;
+	}
+
+	int radius = 1;
+
+	if (((x - 1) * (x - 1) + (y - 1) * (y - 1)) <= radius * radius) {
+		std::cout << "The point is in the square." << std::endl;
+		return 1;
+	}
+	else {
+		std::cout << "The point is not in the square." << std::endl;
+		return 1;
+	}
+
+	return 1;
+}
