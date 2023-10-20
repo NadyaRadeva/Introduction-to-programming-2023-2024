b52bcb6c6069fb291e42dcefc5fd46353912c091
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-20 14:35:36 +0300

    Create VersionA.cpp

---
+++ Practicals/Week 3/Group 5/Exercise 1/VersionA.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main() {
+	int n;
+	std::cout << "Enter a number: ";
+	std::cin >> n;
+	if (!std::cin) {
+		std::cout << "Error! You have entered wrong data!" << std::endl;
+		return 1;
+	}
+
+	int sum = 0;
+
+	for (size_t i = 1; i <= n; ++i) {
+		sum += i;
+	}
+
+	std::cout << "Sum: " << sum << std::endl;
+
+	return 1;
+}
