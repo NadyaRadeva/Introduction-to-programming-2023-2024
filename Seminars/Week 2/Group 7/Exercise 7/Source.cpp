55cbfcbc0fb6f076de3e39e7f244668849496224
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-26 09:13:17 +0300

    Create Source.cpp

---
+++ Seminars/Week 2/Group 7/Exercise 7/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int main() {
+	int x1, y1, radius1, x2, y2, radius2;
+
+	double d = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+
+	if (d <= radius1 - radius2) {
+		std::cout << "The second circle is inside the first one." << std::endl;
+	}
+	else if (d <= radius2 - radius1) {
+		std::cout << "The first circle is inside the second one." << std::endl;
+	}
+	else if (d < radius1 + radius2) {
+		std::cout << "The two circles intersect to each other." << std::endl;
+	}
+	else if (d == radius1 + radius2) {
+		std::cout << "The two circles touch." << std::endl;
+	}
+	else {
+		std::cout << "The two circles do not touch each other." << std::endl;
+	}
+
+	return 1;
+}
