afbbd05819aaeb48ad27084676cc851266e5028b
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-11-01 08:13:48 +0200

    Create VersionB.cpp

---
+++ Workbook/Глава 2 - Основни структури за управление на изчислителния процес/Решени задачи/Задача 35/VersionB.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+int main() {
+	int x1, y1;
+	do {
+		std::cout << "x1 = ";
+		std::cin >> x1;
+		std::cout << "y1 = ";
+		std::cin >> y1;
+	} while (x1 < 1 || x1>8 || y1 < 1 || y1>8);
+
+	int x2, y2;
+	do {
+		std::cout << "x2 = ";
+		std::cin >> x2;
+		std::cout << "y2 = ";
+		std::cin >> y2;
+	} while (x1 < 1 || x2>8 || y2 < 1 || y2>8);
+
+	if ((x1 - 1 == x2 || x1 == x2 || x1 + 1 == x2) && (x1 == x2 || y1 == y2)) {
+		std::cout << "The second point can be reached by a rook." << std::endl;
+	}
+	else {
+		std::cout << "The second point can not be reached by a rook." << std::endl;
+	}
+
+	return 1;
+}
