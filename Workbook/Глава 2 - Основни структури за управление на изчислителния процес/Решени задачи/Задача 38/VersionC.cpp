2cda690bcbb22205271b26224315ff6ab0d95f04
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-11-01 09:23:17 +0200

    Create VersionC.cpp

---
+++ Workbook/Глава 2 - Основни структури за управление на изчислителния процес/Решени задачи/Задача 38/VersionC.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+int main() {
+	std::cout << "Enter the value of X and N (n>=1): ";
+	double x;
+	std::cin >> x;
+	if (!std::cin) {
+		std::cout << "Error! You have entered wrong data!" << std::endl;
+		return 1;
+	}
+
+	int n;
+	std::cin >> n;
+	if (!std::cin) {
+		std::cout << "Error! You have entered wrong data!" << std::endl;
+		return 1;
+	}
+
+	double y = sin(x), s = y;
+
+	for (size_t i = 2; i <= n; ++i) {
+		y = sin(x);
+		s = s + y;
+	}
+
+	std::cout << "s = " << s << std::endl;
+
+	return 1;
+}
