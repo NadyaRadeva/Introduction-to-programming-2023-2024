c8365155d063f18e7bdef405dc6df57426a21dff
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-11-04 22:36:21 +0200

    Create Bonus.cpp

---
+++ Practicals/Week 5/Group 5/Exercise 7/Bonus.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main() {
+	unsigned n;
+	std::cin >> n;
+
+	unsigned mask = 0;
+	while (n) {
+		mask = mask | (1 << (n % 10));
+		n /= 10;
+	}
+	std::cout << ((mask == (1 << 10) - 1) ? "Has all digits" : "Does not have all digits") << std::endl;
+
+	unsigned n;
+	std::cin >> n;
+
+	unsigned mask = 0;
+	while (n) {
+		if ((mask ^ (1 << (n % 10))) < mask) {
+			std::cout << "Not with unique digits" << std::endl;
+			return 0;
+		}
+		mask = mask ^ (1 << (n % 10));
+		n /= 10;
+	}
+	std::cout << "With unique digits" << std::endl;
+
+  return 1;
+}
