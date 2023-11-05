b575b06f2897a64184363eb6867dd58db4d9b4b7
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-11-05 11:14:20 +0200

    Update Source.cpp

--- Practicals/Week 5/Group 5/Exercise 14/Source.cpp
+++ Practicals/Week 5/Group 5/Exercise 14/Source.cpp
@@ -2,43 +2,43 @@
 
 const int MAX_SIZE = 1024;
 
-void read_array(int* arr, unsigned arr_size) {
-  for (int i = 0; i < arr_size; i++) {
+void readArray(int* arr, unsigned arrSize) {
+  for (int i = 0; i < arrSize; i++) {
     std::cout << "Enter element at arr[" << i << "]: " << std::endl;
     std::cin >> arr[i];
   }
 }
 
-unsigned read_array_size(const unsigned max_size) {
-  unsigned arr_size;
+unsigned readArraySize(const unsigned maxSize) {
+  unsigned arrSize;
 
   do {
     std::cout << "Enter array size: " << std::endl;
 
-    std::cin >> arr_size;
-  } while (arr_size > max_size);
+    std::cin >> arrSize;
+  } while (arrSize > maxSize);
 
-  return arr_size;
+  return arrSize;
 }
 
-void print_array(int* arr, unsigned arr_size) {
-  for (int i = 0; i < arr_size; i++) {
+void print_array(int* arr, unsigned arrSize) {
+  for (int i = 0; i < arrSize; i++) {
     std::cout << arr[i] << std::endl;
   }
 }
 
-void swap_elements_in_array(int* arr, int el_1_idx, int el_2_idx) {
+void swapElementsInArray(int* arr, int el_1_idx, int el_2_idx) {
   int temp = arr[el_1_idx];
   arr[el_1_idx] = arr[el_2_idx];
   arr[el_2_idx] = temp;
 }
 
-void sort_array(int* arr, unsigned arr_size) {
+void sortArray(int* arr, unsigned arrSize) {
   // bubble sort
-  for (int i = 0; i < arr_size - 1; i++) {
-    for (int j = i + 1; j < arr_size; j++) {
+  for (int i = 0; i < arrSize - 1; i++) {
+    for (int j = i + 1; j < arrSize; j++) {
       if (arr[i] > arr[j]) {
-        swap_elements_in_array(arr, i, j);
+        swapElementsInArray(arr, i, j);
       }
     }
   }
@@ -47,12 +47,12 @@ void sort_array(int* arr, unsigned arr_size) {
 int main() {
   int arr[MAX_SIZE];
 
-  unsigned arr_size = read_array_size(MAX_SIZE);
+  unsigned arr_size = readArraySize(MAX_SIZE);
 
-  read_array(arr, arr_size);
+  read_array(arr, arrSize);
 
-  sort_array(arr, arr_size);
+  sort_array(arr, arrSize);
 
-  print_array(arr, arr_size);
+  print_array(arr, arrSize);
   return 0;
 }
