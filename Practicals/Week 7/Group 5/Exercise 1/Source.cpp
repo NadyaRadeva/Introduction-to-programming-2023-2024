97d9177fd902a6ce63553063e4210f3986496849
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-11-17 21:40:18 +0200

    Create Source.cpp

---
+++ Practicals/Week 7/Group 5/Exercise 1/Source.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+const unsigned int MAX_ROWS = 50;
+const unsigned int MAX_COLUMNS = 50;
+int matrix[MAX_ROWS][MAX_COLUMNS];
+
+int main() {
+	unsigned int n, m;
+	std::cout << "Enter the number of rows: ";
+	std::cin >> n;
+	if (!std::cin || n < 0 || n > 50) {
+		throw std::runtime_error("Invalid input!");
+	}
+	std::cout << "Enter the number of columns: ";
+	std::cin >> m;
+	if (!std::cin || m < 0 || m > 50) {
+		throw std::runtime_error("Invalid input!");
+	}
+
+	std::cout << "Enter elements in the array: ";
+	for (size_t i = 0; i < n; ++i) {
+		for (size_t j = 0; j < m; ++j) {
+			std::cin >> matrix[i][j];
+		}
+	}
+
+	int sum = 0;
+	for (size_t i = 0; i < n; ++i) {
+		for (size_t j = 0; j < m; ++j) {
+			sum += matrix[i][j];
+		}
+	}
+
+	std::cout << "The sum of all elements is: " << sum << std::endl;
+
+	return 1;
+}
