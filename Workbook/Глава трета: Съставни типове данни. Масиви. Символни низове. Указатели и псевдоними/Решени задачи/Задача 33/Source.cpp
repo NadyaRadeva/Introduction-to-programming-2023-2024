f68eeeabe4fe694242854bee1d9f36d9384205dd
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-11-30 11:06:11 +0200

    Create Source.cpp

---
+++ Workbook/Глава трета: Съставни типове данни. Масиви. Символни низове. Указатели и псевдоними/Решени задачи/Задача 33/Source.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+
+const unsigned int MAX_ROW_AND_COL_SIZE = 10;
+int matrix[MAX_ROW_AND_COL_SIZE][MAX_ROW_AND_COL_SIZE];
+
+void createMatrix(int matrix[][MAX_ROW_AND_COL_SIZE], unsigned int size);
+void printMatrix(int matrix[][MAX_ROW_AND_COL_SIZE], unsigned int size);
+int findProductElBelowSecondaryDiag(int matrix[][MAX_ROW_AND_COL_SIZE], unsigned int size);
+
+void createMatrix(int matrix[][MAX_ROW_AND_COL_SIZE],unsigned int size) {
+	std::cout << "Enter the elements of the matrix: ";
+
+	for (size_t i = 0; i < size; ++i) {
+		for (size_t j = 0; j < size; ++j) {
+			std::cin >> matrix[i][j];
+			if (!std::cin) {
+				throw std::runtime_error("Invalid input!");
+			}
+		}
+	}
+}
+
+void printMatrix(int matrix[][MAX_ROW_AND_COL_SIZE], unsigned int size) {
+	for (size_t i = 0; i < size; ++i) {
+		for (size_t j = 0; j < size; ++j) {
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+int findProductElBelowSecondaryDiag(int matrix[][MAX_ROW_AND_COL_SIZE], unsigned int size) {
+	int pr = 1;
+
+	for (size_t i = 0; i < size; ++i) {
+		for (size_t j = 0; j < size; ++j) {
+			if (i + j > size - 1) {
+				pr *= matrix[i][j];
+			}
+		}
+	}
+
+	return pr;
+}
+
+int main() {
+	std::cout << "Enter the number of rows and columns of the matrix: ";
+	unsigned int size;
+	std::cin >> size;
+	if (!std::cin || size < 0 || size > 10) {
+		throw std::runtime_error("Invalid input!");
+	}
+
+	
+	createMatrix(matrix, size);
+	printMatrix(matrix, size);
+
+	std::cout << "The product of the elements below the secondary diagonal is " << findProductElBelowSecondaryDiag(matrix, size) << "." << std::endl;
+
+	return 1;
+}
