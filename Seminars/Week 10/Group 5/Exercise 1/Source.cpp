4794b5629acc69665d5faef4709f9843eb14bf43
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-12-28 22:04:13 +0200

    Create Source.cpp

---
+++ Seminars/Week 10/Group 5/Exercise 1/Source.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+void toBinary(int number) {
+	if (number == 0) {
+		return;
+	}
+
+	toBinary(number / 2);
+	std::cout << number % 2;
+}
+
+int main() {
+	int number;
+	std::cout << "Enter a number to be turned into binary: ";
+	std::cin >> number;
+	if (!std::cin) {
+		throw std::runtime_error("Invalid input!");
+		return -1;
+	}
+
+	std::cout << "The binary number is: ";
+	toBinary(number);
+
+	std::cout << std::endl;
+
+	return 0;
+}
