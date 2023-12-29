6749063a0384dd88ad99491896577ccd59f71202
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-12-29 08:15:18 +0200

    Create Source.cpp

---
+++ Seminars/Week 10/Group 5/Exercise 3/Source.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+int findSum(int n, int startingValue, int k);
+
+int findSum(int n, int startingValue, int k) {
+	if (startingValue > n)
+	{
+		return 0;
+	}
+
+	return (startingValue + findSum(n, startingValue + k, k));
+}
+
+int main() {
+	int n;
+	std::cout << "Enter the value of N: ";
+	std::cin >> n;
+	if (!std::cin || n < 1) {
+		throw std::runtime_error("Invalid input!");
+		return -1;
+	}
+
+	int k;
+	std::cout << "Enter the value of the step: ";
+	std::cin >> k;
+	if (!std::cin || k > n || k < 1) {
+		throw std::runtime_error("Invalid input!");
+		return -1;
+	}
+
+	std::cout << "The sum with step " << k << " is: " << findSum(n, 1, k);
+
+	std::cout << std::endl;
+
+	return 0;
+}
