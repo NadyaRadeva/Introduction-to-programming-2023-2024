1b683a1ed0fe0aaa30ba20c88567dd9cc61ca121
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2024-01-03 05:27:51 +0200

    Create Source.cpp

---
+++ Practicals/Week 10/Group 5/Exercise 1/Source.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+int exponent(int n, int m) {
+	if (m == 0) {
+		return 1;
+	}
+	
+	return n * exponent(n, m - 1);
+}
+
+int main() {
+	std::cout << "Enter the value of N: ";
+	int n;
+	std::cin >> n;
+	if (!std::cin) {
+		throw std::runtime_error("Invalid input!");
+		return -1;
+	}
+
+	std::cout << "Enter the value of M: ";
+	int m;
+	std::cin >> m;
+	if (!std::cin) {
+		throw std::runtime_error("Invalid input!");
+		return -1;
+	}
+
+	std::cout << "N^M is: " << exponent(n, m) << std::endl;
+
+	return 0;
+}
