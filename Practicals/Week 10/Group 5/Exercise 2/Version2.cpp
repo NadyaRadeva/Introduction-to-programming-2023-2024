f5b183646d2c3164bb142c2f9d200bb6e29ef896
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2024-01-04 11:59:32 +0200

    Create Version2.cpp

---
+++ Practicals/Week 10/Group 5/Exercise 2/Version2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<iomanip>
+
+bool helper(int* arr, int length, int ele, int index) {
+	if (index == length) return false;
+	if (arr[index] == ele) return true;
+	return helper(arr, length, ele, index + 1);
+}
+
+bool member(int* arr, int length, int ele) {
+	return helper(arr, length, ele, 0);
+}
+
+int main() {
+    std::cout << "Enter the size of your array: ";
+    unsigned int sizeArray;
+    std::cin >> sizeArray;
+    if (!std::cin || sizeArray <= 0) {
+        throw std::runtime_error("Invalid input!");
+        return -1;
+    }
+
+    int* array = new int[sizeArray];
+    std::cout << "Enter the elements in your array: ";
+    for (size_t i = 0; i < sizeArray; ++i) {
+        std::cin >> array[i];
+    }
+
+    int number;
+    std::cout << "Enter the number you're looking for: ";
+    std::cin >> number;
+    if (!std::cin) {
+        throw std::runtime_error("Invalid input!");
+        return -1;
+    }
+
+    std::cout << number << " is in the array? - " << std::boolalpha << member(array, sizeArray, number) << std::endl;
+
+    delete[] array;
+
+    return 0;
+}
