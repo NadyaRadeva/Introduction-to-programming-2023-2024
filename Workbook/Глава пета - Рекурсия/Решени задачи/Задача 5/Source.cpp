fccbfb500c230caba110707b56dee587f4022140
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2024-02-04 13:00:17 +0200

    Create Source.cpp

---
+++ Workbook/Глава пета - Рекурсия/Решени задачи/Задача 5/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int gcd(int m, int n);
+
+int gcd(int m, int n) {
+	if (n == 0) {
+		return m;
+	}
+
+	return gcd(n, m % n);
+}
+
+int main() {
+	int m, n;
+	std::cout << "Enter the value of your numbers: ";
+	std::cin >> m >> n;
+	if (!std::cin) {
+		throw std::runtime_error("Error! Invalid input!");
+		return 1;
+	}
+
+	std::cout << "The gcd of m and n is: " << gcd(m, n) << std::endl;
+
+	return 0;
+}
